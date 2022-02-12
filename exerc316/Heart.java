811bfb4a667f12f0aa78d29e35cb8a6b21b854bf
João Henrique <joaokocssis@gmail.com>
2022-02-12 14:54:17 -0300

    utils
    correção

---
+++ exerc316/Heart.java
@@ -0,0 +1,10 @@
+package exerc316;
+
+public class Heart{
+    public static void main(String[] args) {
+        //Utils util = new Utils();
+
+        
+        System.out.println(Utils.calculaIdade("20-10-1971", "dd-MM-yyyy"));
+    }
+}
--- exerc316/Utils.java
+++ exerc316/Utils.java
@@ -9,5 +9,30 @@ import java.io.*;
 
 
 public class Utils {
+    public static int calculaIdade(String dataNasc, String pattern){
+        DateFormat sdf = new SimpleDateFormat(pattern);
+
+        Date dataNascInput = null;
+
+        try{
+        dataNascInput = sdf.parse(dataNasc);
+
+        }catch(Exception e){}
+
+        Calendar dateOfBirth = new GregorianCalendar();
+
+        dateOfBirth.setTime(dataNascInput);
+
+        Calendar today = Calendar.getInstance();
+
+        int age = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
+
+        dateOfBirth.add(Calendar.YEAR, age);
+
+        if (today.before(dateOfBirth)) {
+            age--;
+        }
+        return age;
+    }
     
 }
